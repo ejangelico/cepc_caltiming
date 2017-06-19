6bb76f0a6c0114fe626bd51346795e2ff0805265
Evan Angelico <ejangelico@uchicago.edu>
2017-06-19 09:26:36 +0800

    smearing first try

--- Time/caltiming_analysis_scripts/DataSet.py
+++ Time/caltiming_analysis_scripts/DataSet.py
@@ -1,10 +1,16 @@
 import sys
 import Event
+import cPickle as pickle 
 import matplotlib.pyplot as plt
 
 class DataSet:
 	def __init__(self, events = None, tSmear=0, eSmear=0):
 		self.events = events
+		self.tSmear = tSmear 	#smear 1sigma in ns
+		#fractional energy resolution
+		#takes values between [0.0, 1.0]
+		self.eSmear = eSmear
+
 
 	
 	# Plots the time-average histogram of all the events
@@ -32,5 +38,30 @@ class DataSet:
 		plt.xlabel("Depth into Cal. (mm)")
 		plt.ylabel("Time of Hit (ns)")
 		plt.show()
+
+	def smearAndSave(self, tsm, esm, outfilename):
+		#don't smear this event if it already has been
+		#smeared. Then the full smear is the addition of the
+		#smears in quadrature. Just start from the original file
+		#if(self.tSmear != 0 or self.eSmear != 0):
+		#	print "This data set has already been smeared"
+		#	print "Find the original set and smear that instead"
+		#	return
+
+
+		#make a new list of events
+		smearedEvents = []
+
+		for ev in self.events:
+			newEvent = ev.getSmearedEvent(tsm, esm)
+			smearedEvents.append(newEvent)
+
+		#create new data set
+		smearedData = DataSet(smearedEvents, tsm, esm)
+		#save the dataset to file
+		pickle.dump(smearedData, open(outfilename, 'wb'))
+		return
+
+
 
 
